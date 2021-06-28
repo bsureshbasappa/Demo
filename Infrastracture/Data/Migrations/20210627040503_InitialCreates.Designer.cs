25aacd3c64790e1bc13d6c1f75ce663c4ea9ced4
bsureshbasappa <sureshdug@gmail.com>
2021-06-28 09:38:27 +0300

    End of Section Test

--- Infrastracture/Data/Migrations/20210627040503_InitialCreates.Designer.cs
+++ Infrastracture/Data/Migrations/20210627040503_InitialCreates.Designer.cs
@@ -2,14 +2,16 @@
 using Infrastracture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastracture.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20210627040503_InitialCreates")]
+    partial class InitialCreates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastracture/Data/StoreContextSeed.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastracture.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory){
+            
+            try 
+            {
+                if (!context.ProductBrand.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastracture/Data/SeedData/brands.json");
+                    var brands= JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach(var item in brands)
+                    {
+                        context.ProductBrand.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+
+                if (!context.ProductType.Any())
+                {
+                    var typessData = File.ReadAllText("../Infrastracture/Data/SeedData/types.json");
+                    var types= JsonSerializer.Deserialize<List<ProductType>>(typessData);
+
+                    foreach(var item in types)
+                    {
+                        context.ProductType.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    var ProductsData = File.ReadAllText("../Infrastracture/Data/SeedData/products.json");
+                    var products= JsonSerializer.Deserialize<List<Product>>(ProductsData);
+
+                    foreach(var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}

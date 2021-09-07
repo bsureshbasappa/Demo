9fa7cda7cfe08e6e611d93ececc9695bcff3dac0
bsureshbasappa <sureshdug@gmail.com>
2021-09-07 11:15:08 +0300

    Mid of Section 15

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -2,15 +2,19 @@ using System.Linq;
 using API.Errors;
 using Core.Interfaces;
 using Infrastracture.Data;
+using Infrastracture.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace API.Extensions
 {
     public static class ApplicationServicesExtensions
     {
+        
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
